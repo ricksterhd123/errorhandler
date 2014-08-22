69b880091cb97ab6128ff6792ca4187619eabab9
Ricky <ricky@friendsconnect.nl>
2014-08-23 00:58:36 +0100

    Unnecessary OOP removed and more checks

--- client.lua
+++ client.lua
@@ -1,9 +1,17 @@
+
+--------------------------------------------------
+-- This is a redo of the type() function
+-- with the possibility of multiple parameters.
+--
+-- Only returns true if all variables given are
+-- the same type.
+--------------------------------------------------
 function mType(typ, ...) 
 	if #arg == 0 or type(typ) ~= "string" then 
 		print("Woops")
 		return false 
-	end; 
-	local i = 0; 
+	end
+	local i = 0
 	for k, v in pairs(arg) do 
 		if type(v) == typ then 
 			i = i + 1 
@@ -16,7 +24,10 @@ end
 
 local elements = {
 	window = {0.37, 0.30, 0.25, 0.37, "An error has occured", true},
-	button = {34, 242, 274, 26, "OKAY", false},
+	button = {
+		okayBut = {34, 242, 274, 26, "OKAY", false		},	-- 'OKAY' button, etc
+	},
+
 	memo = {26, 36, 292, 196, false},
 }
 
@@ -36,11 +47,11 @@ function sendError()
 end
 
 local Error = {}
-Error.__index = Error
 
 
 --------------------------------------------------
--- Desc: Create an error.
+-- Desc: Create an error and allows multiple instances
+-- of this gui.
 
 -- Three parameters are needed, the error code
 -- which is used to identify the error then
@@ -53,14 +64,13 @@ function Error:createAndDisplay(id, msgToClient, msgToAdmin, originateFromWindow
 	local oWindow = originateFromWindow or false
 
 	local err = {}
-	setmetatable(err, Error)
 
 	err.id = id
 	err.msg = msgToAdmin
 	table.insert(g_errors, err)
 
 	local w = elements.window
-	local b = elements.button
+	local b = elements.button.okayBut
 	local m = elements.memo
 
 	local window = guiCreateWindow(w[1], w[2], w[3], w[4], w[5], w[6])
@@ -70,20 +80,23 @@ function Error:createAndDisplay(id, msgToClient, msgToAdmin, originateFromWindow
 		guiSetProperty(button, "NormalTextColour", "FFAAAAAA")
 	local memo = guiCreateMemo(m[1], m[2], m[3], m[4], msgToClient, m[5], window)
 		guiMemoSetReadOnly(memo, true)
+
+	assert(window and button and memo, "Couldn't create gui")
 	if not isCursorShowing() then showCursor(true) end
 
 	addEventHandler("onClientGUIClick", button, 
 		function()
 			window:setVisible(false)
-			if not oWindow then 
+			if not oWindow and #g_errors < 2 then 
 				showCursor(false)
 			end
 			destroyElement(window)
 			sendError()
 			err = nil
-		end, false)
+		end, false
+	)
 
-	return
+	return true
 end
 
 function createError(id, cMsg, sMsg)
